445f62543b390d1a858d5af71711678507463613
BerdieVerhoosel <91963456+BerdieVerhoosel@users.noreply.github.com>
2021-12-09 19:28:18 +0100

    Add files via upload

---
+++ bouncer.js
@@ -0,0 +1,29 @@
+
+let age = 20;
+
+if (age < 18) {
+    console.log("You are not old enough, you can not enter..");
+
+} else if (age == 18) {
+    console.log("You are exactly old enough to enter, welcome!")
+
+} else if (age > 18) {
+    console.log("You are old enough, come on in.");
+}
+
+
+let isFemale = false;
+
+if (isFemale) {
+    console.log("Welcome to ladies night!");
+} else {
+    console.log("It's ladies night, see you another time!")
+}
+
+let driverStatus = "lkasdfjli";
+
+if (driverStatus == "bob") {
+    console.log("Drive save and a good night!");
+} else {
+    console.log("You can not drive, call a taxi please.")
+}
